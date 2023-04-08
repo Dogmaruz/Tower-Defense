e0ab3edcb1ebb7f358c7bf1cd0152342aab1513a
Dogmaruz <I@john78.ru>
2023-04-09 02:16:35 +0300

    Добавил стороние assets и сделал карту выбора уровней

---
+++ Tower Defense/Assets/Scripts/Map_Level.cs
@@ -0,0 +1,14 @@
+﻿using SpaceShooter;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class Map_Level : MonoBehaviour
+    {
+        [SerializeField] private Episode m_episode;
+        public void LoadLevel()
+        {
+            LevelSequenceController.Instance.StartEpisode(m_episode);
+        }
+    }
+}
